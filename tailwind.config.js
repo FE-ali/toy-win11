49afc18899c1f095f0d7854dfd347b061c9a0c5c
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-11-17 13:44:19 +0800

    build: 添加部分tailwind自定义配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        desktopImg0: "url('images/wallpaper/img0.jpg')",
+      },
+    },
+    colors: {
+      'task-bar-bg': '#f3f3f3d9',
+      'icon-bg': '#fefefe00',
+      'icon-hover-bg': '#ffffffcc',
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      scale: ['active'],
+    },
   },
   plugins: [],
-}
+};
