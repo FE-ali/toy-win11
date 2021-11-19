355c0618d6be6ee77df3a6d7c1bf60980a8dbbb4
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-11-19 11:50:12 +0800

    build: 添加部分tailwind自定义配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,27 @@ module.exports = {
       dropShadow: {
         battery: '1px 1px #fefefe',
       },
+      height: {
+        'desktop-height': 'calc(100vh - 2.5rem)',
+      },
+      border: {
+        'side-panel-border': '.1px solid rgba(17,17,17,.1)',
+      },
+      width: {
+        90: '22.5rem',
+      },
     },
     colors: {
       'task-bar-bg': '#f3f3f3d9',
       'icon-bg': '#fefefe00',
       'icon-hover-bg': '#ffffffcc',
+      'side-panel-bg': '#f2f2f2e6',
+      'side-panel-btn-bg': '#fbfbfb',
+      'side-panel-border': '#1111111a',
+      'side-panel-slider': '#0067c0',
+    },
+    borderWidth: {
+      '01': '0.1px',
     },
   },
   variants: {
